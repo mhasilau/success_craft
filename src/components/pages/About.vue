376b1dfc3688d4c9e7833d2072987c8d9517862e
mhasilau <mikhail.hasilau@gmail.com>
2023-10-06 15:31:40 +0300

    home + about

--- src/components/pages/About.vue
+++ src/components/pages/About.vue
@@ -1,11 +1,71 @@
 <template>
-  <p>AboutPage</p>
+  <div class="about">
+    <div class="text">
+      <div class="desc">
+        <h1>About us</h1>
+        <div class="demo-collapse">
+          <el-collapse accordion v-for="item in items">
+            <el-collapse-item
+                :title=item.title
+                :name=item.id
+            >
+              <div>{{item.body}}</div>
+            </el-collapse-item>
+          </el-collapse>
+        </div>
+      </div>
+      <div class="buttons">
+        <Button :type="'primary'">Read more</Button>
+        <Button :type="'text'">Contact Us</Button>
+      </div>
+
+    </div>
+    <div class="video">
+      <iframe
+          src="https://www.youtube.com/embed/2gwDjMqJepQ?si=au_E6La_oxdye3gz"
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen></iframe>
+    </div>
+  </div>
 </template>
 
 <script>
+import Button from '@/components/shared/Buttons.vue'
+import { about } from '@/mock/about-page/about'
 export default {
 	name: 'AboutPage',
+  components: {
+    Button,
+  },
+  data() {
+    return {
+      items: about
+    }
+  }
 }
 </script>
 
-<style></style>
+<style scoped lang="scss">
+.about {
+  display: flex;
+  justify-content: space-evenly;
+  //width: 100%;
+
+  .text {
+    width: 50%;
+
+    .buttons {
+      display: flex;
+    }
+  }
+
+  .video {
+    width: 40%;
+    iframe {
+      width: 100%;
+      height: 45%;
+    }
+  }
+}
+</style>
---
+++ src/components/shared/Buttons.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="btns">
+		<el-button class="button" :type=type>
+      <slot></slot>
+    </el-button>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Button',
+  props: {
+    type: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.btns {
+  .button {
+    border: 1px solid #409eff;
+    border-radius: 7px;
+    width: 150px;
+    margin-right: 20px;
+    margin-top: 10px;
+  }
+}
+
+</style>
