5619ba2d08f8fe040165be5d8bee0e18d9544a11
mhasilau <mikhail.hasilau@gmail.com>
2023-10-08 12:23:20 +0300

    footer

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="footer">
+    <p>
+      ©2015-2022 Success Craft
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppFooter'
+}
+</script>
+
+<style lang="scss" scoped>
+  .footer {
+    display: flex;
+    justify-content: center;
+    height: 100px;
+    background-color: #3d3d3d;
+    color: white;
+    align-items: center;
+  }
+</style>
